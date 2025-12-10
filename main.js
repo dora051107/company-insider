54cf7cb95dbe47b37ac1f41efe7f402d11433232
dora051107 <alexandraandres008@gmail.com>
2025-12-10 13:23:26 +0000

    JS-DOM

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ console.log("Admins:", filterUsersByRole(users, "Admin"));
 console.log("User Names:", extractUserNames(users));
 console.log("User with ID 2:", filterUsersById(users, 2));
 
+function renderUserList() {
+    const userList = document.getElementById("user-list");
+    userList.innerHTML ="";
+    users.forEach(user => {
+        const li = document.createElement("li");
+        li.textContent = '${user.name} (${user.role})';
+        userList.appendChild(li);
+    });
+}
+
+const toggleButton = document.getElementById("toggle-dark-mode");
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+const userForm = document.getElementById("user-form");
+userForm.addEventListener("submit", (Event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value ;
+    const role = document.getElementById("role").value ;
+    const email = document.getElementById("email").value ;
+    console.log({ name, role, email });
+});
